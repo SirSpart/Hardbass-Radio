0f49e79284f1d922f8192595b0f9babd42543544
Rainbow <35144413+rainbownotfound@users.noreply.github.com>
2018-08-04 21:51:10 +0200

    index.js - Fixed Bugs, Added Command
    +Added Command "!h leave"
    >Fixed Bugs

--- index.js
+++ index.js
@@ -3,46 +3,59 @@ const http = require('http')
 const express = require('express')
 const app = express()
 app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received")
   response.sendStatus(200)
-})
-app.listen(process.env.PORT)
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`)
-}, 280000)
+});
+app.listen(process.env.PORT);
 
+//randomSelection by RainbowNotFound
+function randomSelection(choices) {
+  return choices[Math.floor(Math.random() * choices.length)];
+};
 
 //and now the actual bot
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Songs = ["Cheeki Breeki"]
+const Songs = ["https://cdn.glitch.com/d63e88df-e942-48e1-9e09-9b1d9df3f511%2FCheeki%20Breeki.mp3?1533410514541"];
 
-client.login(process.env.token)
+client.on("ready", () => {
+  console.log(`I'm ready daddy!`);
+
+  client.user.setPresence({ game: { name: "Use !h join!", type: "PLAYING" } });
+})
 
 client.on('message', message => {
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
   if (!message.guild) return
 
-  if (message.content === '!h join') {
+  if (message.content === '!h join' || message.content === `<@${client.user.id}> join`) {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
         .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!')
-          let RandomSong = randomSelection(Songs)
-          const dispatcher = connection.playFile("./songs/[RandomSong].mp3")
+          message.reply(`I have successfully connected to the channel **${message.member.voiceChannel.name}**!`);
+          let RandomSong = randomSelection(Songs);
+          const dispatcher = connection.playStream(RandomSong);
+
+          dispatcher.on("end", end => {
+            let RandomSong = randomSelection(Songs);
+            connection.playStream(RandomSong);
+          })
         })
         .catch(console.log)
     } else {
-      message.reply('You need to join a voice channel first!')
+      message.reply('You need to join a voice channel first!');
+    }
+  } else if (message.content === "!h leave" || message.content === `<@${client.user.id}> leave`) {
+    if(message.member.voiceChannel) {
+      message.member.voiceChannel.leave();
+
+      message.channel.send(`I have successfully disconnected from the channel **${message.member.voiceChannel.name}**!`);
+    } else {
+      message.channel.send(`I could not find your voice channel...`);
     }
   }
-})
+});
 
-
-//randomSelection by RainbowNotFound
-function randomSelection(choices) {
-  return choices[Math.floor(Math.random() * choices.length)]
-}
+client.login(process.env.TOKEN);
